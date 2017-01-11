b47921453717991a8da443cc4cf315198927fc42
Chau Nguyen <chautnguyen96@gmail.com>
2017-01-10 16:45:16 -0800

    Simple refactoring of Background.js

--- client/src/components/YelpWithWeather/Background.js
+++ client/src/components/YelpWithWeather/Background.js
@@ -5,57 +5,59 @@ import clearNight from '../../img/bg/clear-night.jpg';
 import cloudy from '../../img/bg/cloudy.jpg';
 import cloudyDay from '../../img/bg/partly-cloudy-day.jpg';
 import cloudyNight from '../../img/bg/partly-cloudy-night.jpg';
+import rainy from '../../img/bg/rain.jpg';
 
 class Background extends Component {
+  constructor(props) {
+    super(props);
+    this.getBackground = this.getBackground.bind(this);
+  }
+
   getBackground(icon) {
     // console.log('icon:', icon);
-    if (icon === 'empty') return;
+    if (icon === 'empty') return {};
+
+    let bgStyle = {
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+    };
+
+    let bgImg = {};
+
     switch (icon) {
       case 'clear-day':
-        return {
-          backgroundImage: `url(${clearDay})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-        };
+        bgImg = { backgroundImage: `url(${clearDay})` };
+        break;
       case 'clear-night':
-        return {
-          backgroundImage: `url(${clearNight})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-        };
+        bgImg = { backgroundImage: `url(${clearNight})` };
+        break;
       case 'cloudy':
-        return {
-          backgroundImage: `url(${cloudy})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-        };
+        bgImg = { backgroundImage: `url(${cloudy})` };
+        break;
       case 'partly-cloudy-day':
-        return {
-          backgroundImage: `url(${cloudyDay})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-        };
+        bgImg = { backgroundImage: `url(${cloudyDay})` };
+        break;
       case 'partly-cloudy-night':
-        return {
-          backgroundImage: `url(${cloudyNight})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-        };
+        bgImg = { backgroundImage: `url(${cloudyNight})` };
+        break;
+      case 'rain':
+        bgImg = { backgroundImage: `url(${rainy})` };
+        break;
+      default:
+        console.log('no background img available; pls report');
     }
+
+    return { ...bgStyle, ...bgImg };
   }
 
   render() {
     const { icon } = this.props;
-    const zeIcon = this.getBackground(icon);
+    const bgStyle = this.getBackground(icon);
     return (
       <div
         className="weather-background"
-        style={zeIcon}
+        style={bgStyle}
       />
     );
   }
